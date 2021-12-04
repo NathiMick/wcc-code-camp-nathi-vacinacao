1f33ad22c3f30f3e3930d00455808a631e9be104
Nathi <micheloni@gmail.com>
2021-12-04 04:52:12 -0300

    deploying heroku

--- src/app.js
+++ src/app.js
@@ -23,6 +23,16 @@ app
 .use('/vaccines', vaccinesRoutes)
 .use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs))
 
+.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+      "Access-Control-Allow-Headres",
+      "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+})
+
+
 
 
 //Access permissions for Front-End
